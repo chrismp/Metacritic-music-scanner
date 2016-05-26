281e0f0b30b8b1f816f7246889c6c1b79a9152fa
Chris <chris@debian.personal>
2016-05-26 17:37:17 -0400

    add launcher

---
+++ launch.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup ruby scanner.rb > output.log 2>&1 &
--- scanner.rb
+++ scanner.rb
@@ -39,6 +39,7 @@ csvInfoHash= {
 	criticsCSV => [
 		"Critic",
 		"AlbumURL",
+		"Date",
 		"Score"
 	],
 	genresCSV => [
@@ -55,56 +56,87 @@ csvInfoHash.each{|fileName,headersArray|
 	end
 }
 
-# agent=					Mechanize.new
-# agent.user_agent_alias=	"Linux Firefox"
+agent=					Mechanize.new
+agent.user_agent_alias=	"Linux Firefox"
 
-# baseURL=		"http://www.metacritic.com"
-# letterArray=	('a'..'z').map{|x| x}
-# letterArray.insert(0,'9')
-# letterArray.each{|ltr|
-# 	pgNum=	0
-# 	loop{  
-# 		albumDirectoryURL=	baseURL+"/browse/albums/release-date/available/date?page="+pgNum.to_s
-# 		albumDirectoryPage=	openURL(agent,albumDirectoryURL)
-# 		listProducts=		albumDirectoryPage.css(".list_products")	# `ol` containing `li` elements containing links to album pages
-# 		if listProducts.length==0
-# 			break
-# 		end
+baseURL=		"http://www.metacritic.com"
+pgNum=			0
+loop{  
+	albumDirectoryURL=	baseURL+"/browse/albums/release-date/available/date?page="+pgNum.to_s
+	albumDirectoryPage=	openURL(agent,albumDirectoryURL)
+	listProducts=		albumDirectoryPage.css(".list_products")	# `ol` containing `li` elements containing links to album pages
+	if listProducts.length==0
+		break
+	end
 
-# 		listProducts.css('a').each{|a|
-# 			albumURL=	baseURL+a["href"]
-# 			albumPage=	openURL(agent,albumURL)
-# 			album=		textStrip(albumPage.css(".product_title")[0])
-# 			artist=		textStrip(albumPage.css(".product_artist a")[0])
-# 			artistHref=	albumPage.css(".product_artist a")[0].attr("href")
-# 			label=		textStrip(albumPage.css(".product_company .data")[0])
-# 			labelHref=	albumPage.css(".publisher a")[0].attr("href")
-# 			genres=		albumPage.css(".product_genre .data").map{|d|
-# 				textStrip(d)
-# 			}
-# 			summary=	textStrip(albumPage.css(".product_summary .data span"))
-# 			metascore=	textStrip(albumPage.css(".metascore_summary span[itemprop='ratingValue']")[0])
-# 			criticScores=textStrip(albumPage.css(".metascore_summary span[itemprop='reviewCount']")[0])
-# 			userScore=	textStrip(albumPage.css(".userscore_wrap div.user")[0])
-# 			userScore=	userScore == "tbd" ? nil : userScore
-# 			userScores=	nil
-# 			if userScore != nil
-# 				userScores=	textStrip(albumPage.css(".userscore_wrap .count a")[0]).gsub(" Ratings",'')	
-# 			end
-			
-# 			p album,artist,artistHref,label,labelHref,genres,summary,metascore,criticScores,userScore,userScores,"=="
+	listProducts.css('a').each{|a|
+		albumURL=	baseURL+a["href"]
+		albumPage=	openURL(agent,albumURL)
+		album=		textStrip(albumPage.css(".product_title")[0])
+		artist=		textStrip(albumPage.css(".product_artist a")[0])
+		artistHref=	albumPage.css(".product_artist a")[0].attr("href")
+		label=		textStrip(albumPage.css(".product_company .data")[0])
+		labelHref=	albumPage.css(".publisher a")[0].attr("href")
+		summary=	textStrip(albumPage.css(".product_summary .data span"))
+		metascore=	textStrip(albumPage.css(".metascore_summary span[itemprop='ratingValue']")[0])
+		criticScores=textStrip(albumPage.css(".metascore_summary span[itemprop='reviewCount']")[0])
+		userScore=	textStrip(albumPage.css(".userscore_wrap div.user")[0])
+		userScore=	userScore == "tbd" ? nil : userScore
+		userScores=	nil
+		if userScore != nil
+			userScores=	textStrip(albumPage.css(".userscore_wrap .count a")[0]).gsub(" Ratings",'')	
+		end
 
-# 			criticsURL=	albumURL+"/critic-reviews"
-# 			criticsPage=openURL(agent,criticsURL)
-# 			criticsPage.css(".critic_review").each{|reviewTag|
-# 				critic=		textStrip(reviewTag.css(".source")[0])
-# 				reviewDate=	textStrip(reviewTag.css(".date")[0])
-# 				criticScore=textStrip(reviewTag.css(".review_grade")[0])
-# 				p critic,reviewDate,criticScore
-# 				p "==="
-# 			}
-# 		}
-# 		p "========"
-# 		pgNum+=1
-# 	}
+		CSV.open(albumsCSV,'a') do |csv|
+			csv << [
+				a["href"],
+				album,
+				artistHref,
+				label,
+				labelHref,
+				summary,
+				metascore,
+				criticScores,
+				userScore,
+				userScores
+			]
+		end
+		CSV.open(artistsCSV,'a') do |csv|
+			csv << [
+				artist,
+				artistHref
+			]
+		end
+		albumPage.css(".product_genre .data").each{|genre|
+			CSV.open(genresCSV,'a') do |csv|
+				csv << [
+					genre,
+					a["href"]
+				]
+			end
+		}
+		
+		p album,artist,artistHref,label,labelHref,summary,metascore,criticScores,userScore,userScores,"=="
+
+		criticsURL=	albumURL+"/critic-reviews"
+		criticsPage=openURL(agent,criticsURL)
+		criticsPage.css(".critic_review").each{|reviewTag|
+			critic=		textStrip(reviewTag.css(".source")[0])
+			reviewDate=	textStrip(reviewTag.css(".date")[0])
+			criticScore=textStrip(reviewTag.css(".review_grade")[0])
+			CSV.open(criticsCSV,'a') do |csv|
+				csv << [
+					critic,
+					a["href"],
+					reviewDate,
+					criticScore
+				]
+			end
+			p critic,reviewDate,criticScore
+			p "==="
+		}
+	}
+	p "========"
+	pgNum+=1
+}
 # }
